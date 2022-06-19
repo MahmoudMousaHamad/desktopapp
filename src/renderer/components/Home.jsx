2a1532aeb56e80ef4b4f8811b770ba2239be7318
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-06-19 05:47:40 -0500

    init

--- src/renderer/components/Home.jsx
+++ src/renderer/components/Home.jsx
@@ -1,21 +1,21 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 export default () => {
-    const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
 
-    /**
-     * Start scraper and send user to main process
-     */
-    const start = () => {
-        window.electron.ipcRenderer.send('start-scraper', user);
-    }
+  /**
+   * Start scraper and send user to main process
+   */
+  const start = () => {
+    window.electron.ipcRenderer.send('start-scraper', user);
+  };
 
-    console.log(user);
-    return (
-        <>        
-            <h1>Welcome home!</h1>
-            {user && <button onClick={start}>Start Scraper</button>}
-        </>
-    );
-}
+  console.log(user);
+  return (
+    <>
+      <h1>Welcome home!</h1>
+      {user && <button type='button' onClick={start}>Start Scraper</button>}
+    </>
+  );
+};
