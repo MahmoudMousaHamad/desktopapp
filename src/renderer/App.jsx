da53ea80f55984f4845e7f63e0f979eef6cd97d8
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-07-26 20:53:08 -0500

    init

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -149,7 +149,7 @@ const App = () => {
 							<Typography level="h2" fontWeight={700}>
 								JobApplier
 							</Typography>
-							v0.1.4
+							v0.1.5
 						</Box>
 						<Box sx={{ display: "flex", flexDirection: "row", gap: 1.5 }}>
 							<IconButton
