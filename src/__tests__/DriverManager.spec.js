86a9eedfd0c9080f7f31d0d2c955bfee235cc801
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-11-01 12:46:20 -0500

    fix import

--- src/__tests__/DriverManager.spec.js
+++ src/__tests__/DriverManager.spec.js
@@ -1,14 +1,14 @@
 import fs from "fs";
 
 import { downloadChromeDriver } from "../main/driver/Manager";
-import { chromeDriverPath } from "../main/lib/OS";
+import OS from "../main/lib/OS";
 
 test("downloadChromeDriver() downloads chrome driver", async () => {
-	if (fs.existsSync(chromeDriverPath)) {
-		fs.unlinkSync(chromeDriverPath);
+	if (fs.existsSync(OS.chromeDriverPath)) {
+		fs.unlinkSync(OS.chromeDriverPath);
 	}
-	expect(fs.existsSync(chromeDriverPath)).toBeFalsy();
+	expect(fs.existsSync(OS.chromeDriverPath)).toBeFalsy();
 	await downloadChromeDriver();
 	await new Promise((resolve) => setTimeout(resolve, 10000));
-	expect(fs.existsSync(chromeDriverPath)).toBeTruthy();
+	expect(fs.existsSync(OS.chromeDriverPath)).toBeTruthy();
 });
