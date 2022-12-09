df3ffea418a50b542035fecdeb50e856aeb14a8d
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2022-12-09 14:35:55 -0600

    mac

---
+++ package_mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npx cross-env \
+DO_KEY_ID=OKJRQ3JKZHYSMDJP3PZZ \
+DO_SECRET_KEY=WLNAC1hvaGaAiXwqaiTmL3hiPvKq4ieagH2C1sy5ozo \
+APPLE_ID=mahmoudmousahamad@gmail.com \
+APPLE_ID_PASS=hsjb-ojek-ajwr-bwlz \
+npm run package --mac
